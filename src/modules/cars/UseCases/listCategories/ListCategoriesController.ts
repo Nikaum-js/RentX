8bf3f917c0cf105c1de74b20b295a5fa72b8ca9c
Nikolas-as <nikkolas21082003@gmail.com>
2021-10-01 13:07:15 -0300

    updates :rocket:

---
+++ src/modules/cars/UseCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+
+class ListCategoriesController {
+  constructor(private ListCategoriesUseCase: ListCategoriesUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const all = this.ListCategoriesUseCase.execute();
+
+    return response.json(all);
+  }
+  
+}
+
+export { ListCategoriesController }
--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,12 +1,23 @@
-import { Category } from "../model/Category"
+import { Category } from "../../model/Category"
 
-import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesRepository"
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository"
 
 class CategoriesRepository implements ICategoriesRepository { 
   private  categories: Category[] = [];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
   }
 
   create( { name, description }: ICreateCategoryDTO) {
