ce5136dd5d8bc0039516d863e4c6b741417d3762
Nikolas-as <nikkolas21082003@gmail.com>
2021-10-01 20:06:48 -0300

    updates :rocket:

---
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+
+class ImportCategoryUseCase {
+
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) =>{
+      console.log(line);
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
