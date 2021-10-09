7ba9cff7836dfe28133ddb632a149536eeda75e4
Nikolas-as <nikkolas21082003@gmail.com>
2021-10-09 18:07:00 -0300

    add typeorm and postgres :pencil:

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 
-const app = express();
+import "./database";
 
+const app = express();
 
 app.use(express.json());
 
