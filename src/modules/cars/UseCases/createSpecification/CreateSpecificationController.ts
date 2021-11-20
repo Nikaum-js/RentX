50c2ce921973093c81bc3c09ceeb8f467b88f572
Nikolas.Dev <nikkolas21082003@gmail.com>
2021-11-20 20:17:24 -0300

    Update CreateSpecificationController.ts

--- src/modules/cars/UseCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/UseCases/createSpecification/CreateSpecificationController.ts
@@ -8,7 +8,6 @@ class CreateSpecificationController {
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
-
     this.createSpecificationUseCase.execute({ name, description});
   
     return response.status(201).send();
